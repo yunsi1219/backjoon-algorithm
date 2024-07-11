4eff3d16811c57c87cc67458ca00c3099c79aad4
yunsi1219 <orenjh1@naver.com>
2024-07-11 23:40:42 +0900

    1day

---
+++ 240711/src/Stage1/Prob1.java
@@ -0,0 +1,7 @@
+package Stage1;
+
+public class Prob1 {
+ public static void main(String[]args) {
+	 System.out.print("Hello World!");
+ }
+}
---
+++ 240711/src/Stage1/Prob2.java
@@ -0,0 +1,17 @@
+package Stage1;
+
+import java.util.*;
+
+public class Prob2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		System.out.print(A+B);
+
+	}
+}
